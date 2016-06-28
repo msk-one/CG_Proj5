6add93bc5d79a7368f1b1d0e6e029407359dd7c0
Michal Szklarski <msk@nex10.pl>
2016-06-28 23:20:49 +0200

    Init.

---
+++ CG_Proj5/CG_Proj5/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CG_Proj5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void downButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void upButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
