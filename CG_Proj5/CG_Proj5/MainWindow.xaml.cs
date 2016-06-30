562f933d261614b8f3f7c8f33d54167b9449d686
Michal Szklarski <msk@nex10.pl>
2016-07-01 00:03:47 +0200

    Polishing

--- CG_Proj5/CG_Proj5/MainWindow.xaml.cs
+++ CG_Proj5/CG_Proj5/MainWindow.xaml.cs
@@ -10,155 +10,60 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
+using SharpDX;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CG_Proj5
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-    /// 
-
-    public class Camera
-    {
-        public Vector3D Position { get; set; }
-        public Vector3D Target { get; set; }
-    }
-    public class Mesh
-    {
-        public string Name { get; set; }
-        public Vector3D[] Vertices { get; private set; }
-        public Vector3D Position { get; set; }
-        public Vector3D Rotation { get; set; }
-
-        public Mesh(string name, int verticesCount)
-        {
-            Vertices = new Vector3D[verticesCount];
-            Name = name;
-        }
-    }
-
-    public class Device
-    {
-        private byte[] backBuffer;
-        private WriteableBitmap bmp;
-
-        public Device(WriteableBitmap bmp)
-        {
-            this.bmp = bmp;
-            // the back buffer size is equal to the number of pixels to draw
-            // on screen (width*height) * 4 (R,G,B & Alpha values). 
-            backBuffer = new byte[bmp.PixelWidth * bmp.PixelHeight * 4];
-        }
-
-        // This method is called to clear the back buffer with a specific color
-        public void Clear(byte r, byte g, byte b, byte a)
-        {
-            for (var index = 0; index < backBuffer.Length; index += 4)
-            {
-                // BGRA is used by Windows instead by RGBA in HTML5
-                backBuffer[index] = b;
-                backBuffer[index + 1] = g;
-                backBuffer[index + 2] = r;
-                backBuffer[index + 3] = a;
-            }
-        }
-
-        // Once everything is ready, we can flush the back buffer
-        // into the front buffer. 
-        public void Present()
-        {
-            using (var stream = bmp.)
-            {
-                // writing our byte[] back buffer into our WriteableBitmap stream
-                stream.Write(backBuffer, 0, backBuffer.Length);
-            }
-            // request a redraw of the entire bitmap
-           
-        }
-
-        // Called to put a pixel on screen at a specific X,Y coordinates
-        public void PutPixel(int x, int y, Color4 color)
-        {
-            // As we have a 1-D Array for our back buffer
-            // we need to know the equivalent cell in 1-D based
-            // on the 2D coordinates on screen
-            var index = (x + y * bmp.PixelWidth) * 4;
-
-            backBuffer[index] = (byte)(color.Blue * 255);
-            backBuffer[index + 1] = (byte)(color.Green * 255);
-            backBuffer[index + 2] = (byte)(color.Red * 255);
-            backBuffer[index + 3] = (byte)(color.Alpha * 255);
-        }
-
-        // Project takes some 3D coordinates and transform them
-        // in 2D coordinates using the transformation matrix
-        public Vector2 Project(Vector3 coord, Matrix transMat)
-        {
-            // transforming the coordinates
-            var point = Vector3.TransformCoordinate(coord, transMat);
-            // The transformed coordinates will be based on coordinate system
-            // starting on the center of the screen. But drawing on screen normally starts
-            // from top left. We then need to transform them again to have x:0, y:0 on top left.
-            var x = point.X * bmp.PixelWidth + bmp.PixelWidth / 2.0f;
-            var y = -point.Y * bmp.PixelHeight + bmp.PixelHeight / 2.0f;
-            return (new Vector2(x, y));
-        }
-
-        // DrawPoint calls PutPixel but does the clipping operation before
-        public void DrawPoint(Vector2 point)
-        {
-            // Clipping what's visible on screen
-            if (point.X >= 0 && point.Y >= 0 && point.X < bmp.PixelWidth && point.Y < bmp.PixelHeight)
-            {
-                // Drawing a yellow point
-                PutPixel((int)point.X, (int)point.Y, new Color4(1.0f, 1.0f, 0.0f, 1.0f));
-            }
-        }
-
-        // The main method of the engine that re-compute each vertex projection
-        // during each frame
-        public void Render(Camera camera, params Mesh[] meshes)
-        {
-            // To understand this part, please read the prerequisites resources
-            var viewMatrix = Matrix.LookAtLH(camera.Position, camera.Target, Vector3.UnitY);
-            var projectionMatrix = Matrix.PerspectiveFovRH(0.78f,
-                                                           (float)bmp.PixelWidth / bmp.PixelHeight,
-                                                           0.01f, 1.0f);
-
-            foreach (Mesh mesh in meshes)
-            {
-                // Beware to apply rotation before translation 
-                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y,
-                                                              mesh.Rotation.X, mesh.Rotation.Z) *
-                                  Matrix.Translation(mesh.Position);
-
-                var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
-
-                foreach (var vertex in mesh.Vertices)
-                {
-                    // First, we project the 3D coordinates into the 2D space
-                    var point = Project(vertex, transformMatrix);
-                    // Then we can draw on screen
-                    DrawPoint(point);
-                }
-            }
-        }
-    }
 
     public partial class MainWindow : Window
     {
-        private Vector3D currCameraLoc;
-        private List<Vector3D> currObject; 
+        public WriteableBitmap writeableBmp;
+        private Screen _screen;
+        private Mesh mesh;
+        private Camera camera;
 
         public MainWindow()
         {
             InitializeComponent();
-            currCameraLoc = new Vector3D(20,20,20);
+            //currCameraLoc = new Vector3(20,20,20);
+            writeableBmp = BitmapFactory.New(850, 726);
+            mesh = new Mesh(8, 12);
+            
+            camera = new Camera();
+            _screen = new Screen(writeableBmp);
+
+            mainImage.Source = writeableBmp;
+
+            mesh.Vertices[0] = new Vector3(-1, 1, 1);
+            mesh.Vertices[1] = new Vector3(1, 1, 1);
+            mesh.Vertices[2] = new Vector3(-1, -1, 1);
+            mesh.Vertices[3] = new Vector3(1, -1, 1);
+            mesh.Vertices[4] = new Vector3(-1, 1, -1);
+            mesh.Vertices[5] = new Vector3(1, 1, -1);
+            mesh.Vertices[6] = new Vector3(1, -1, -1);
+            mesh.Vertices[7] = new Vector3(-1, -1, -1);
+
+            mesh.Faces[0] = new Face { v1 = 0, v2 = 1, v3 = 2 };
+            mesh.Faces[1] = new Face { v1 = 1, v2 = 2, v3 = 3 };
+            mesh.Faces[2] = new Face { v1 = 1, v2 = 3, v3 = 6 };
+            mesh.Faces[3] = new Face { v1 = 1, v2 = 5, v3 = 6 };
+            mesh.Faces[4] = new Face { v1 = 0, v2 = 1, v3 = 4 };
+            mesh.Faces[5] = new Face { v1 = 1, v2 = 4, v3 = 5 };
+
+            mesh.Faces[6] = new Face { v1 = 2, v2 = 3, v3 = 7 };
+            mesh.Faces[7] = new Face { v1 = 3, v2 = 6, v3 = 7 };
+            mesh.Faces[8] = new Face { v1 = 0, v2 = 2, v3 = 7 };
+            mesh.Faces[9] = new Face { v1 = 0, v2 = 4, v3 = 7 };
+            mesh.Faces[10] = new Face { v1 = 4, v2 = 5, v3 = 6 };
+            mesh.Faces[11] = new Face { v1 = 4, v2 = 6, v3 = 7 };
+
+            camera.Position = new Vector3(0, 0, 10.0f);
+            camera.Target = Vector3.Zero;
+
         }
+
 
         private void downButton_Click(object sender, RoutedEventArgs e)
         {
@@ -172,49 +77,106 @@ namespace CG_Proj5
 
         private void leftButton_rot_Click(object sender, RoutedEventArgs e)
         {
-
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y - 0.11f, mesh.Rotation.Z);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
 
         private void upButton_rot_Click(object sender, RoutedEventArgs e)
         {
-
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.11f, mesh.Rotation.Y, mesh.Rotation.Z);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
 
         private void rightButton_rot_Click(object sender, RoutedEventArgs e)
         {
-
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y + 0.11f, mesh.Rotation.Z);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
 
-        //Sphere
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void downButton_rot_Click(object sender, RoutedEventArgs e)
         {
-
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X - 0.11f, mesh.Rotation.Y, mesh.Rotation.Z);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
+
 
         //Cone
         private void button_Copy_Click(object sender, RoutedEventArgs e)
         {
+            mesh = new Mesh(7, 6);
+            mesh.Vertices[0] = new Vector3(0, 2, 0);
+            mesh.Vertices[1] = new Vector3(-1, -1, 1);
+            mesh.Vertices[2] = new Vector3(-2, -1, 0);
+            mesh.Vertices[3] = new Vector3(-1, -1, -1);
+            mesh.Vertices[4] = new Vector3(1, -1, -1);
+            mesh.Vertices[5] = new Vector3(2, -1, 0);
+            mesh.Vertices[6] = new Vector3(1, -1, 1);
 
+            mesh.Faces[0] = new Face { v1 = 0, v2 = 1, v3 = 6 };
+            mesh.Faces[1] = new Face { v1 = 2, v2 = 1, v3 = 0 };
+            mesh.Faces[2] = new Face { v1 = 2, v2 = 3, v3 = 0 };
+            mesh.Faces[3] = new Face { v1 = 3, v2 = 4, v3 = 0 };
+            mesh.Faces[4] = new Face { v1 = 4, v2 = 5, v3 = 0 };
+            mesh.Faces[5] = new Face { v1 = 5, v2 = 6, v3 = 0 };
+
+            _screen.Clear(0, 0, 0, 255);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
 
         //Cube
         private void button_Copy1_Click(object sender, RoutedEventArgs e)
         {
-            var mesh = new Mesh("Cube", 8);
-            mesh.Vertices[0] = new Vector3D(-1, 1, 1);
-            mesh.Vertices[1] = new Vector3D(1, 1, 1);
-            mesh.Vertices[2] = new Vector3D(-1, -1, 1);
-            mesh.Vertices[3] = new Vector3D(-1, -1, -1);
-            mesh.Vertices[4] = new Vector3D(-1, 1, -1);
-            mesh.Vertices[5] = new Vector3D(1, 1, -1);
-            mesh.Vertices[6] = new Vector3D(1, -1, 1);
-            mesh.Vertices[7] = new Vector3D(1, -1, -1);
+            mesh = new Mesh(8, 12);
+            mesh.Vertices[0] = new Vector3(-1, 1, 1);
+            mesh.Vertices[1] = new Vector3(1, 1, 1);
+            mesh.Vertices[2] = new Vector3(-1, -1, 1);
+            mesh.Vertices[3] = new Vector3(1, -1, 1);
+            mesh.Vertices[4] = new Vector3(-1, 1, -1);
+            mesh.Vertices[5] = new Vector3(1, 1, -1);
+            mesh.Vertices[6] = new Vector3(1, -1, -1);
+            mesh.Vertices[7] = new Vector3(-1, -1, -1);
+
+            mesh.Faces[0] = new Face { v1 = 0, v2 = 1, v3 = 2 };
+            mesh.Faces[1] = new Face { v1 = 1, v2 = 2, v3 = 3 };
+            mesh.Faces[2] = new Face { v1 = 1, v2 = 3, v3 = 6 };
+            mesh.Faces[3] = new Face { v1 = 1, v2 = 5, v3 = 6 };
+            mesh.Faces[4] = new Face { v1 = 0, v2 = 1, v3 = 4 };
+            mesh.Faces[5] = new Face { v1 = 1, v2 = 4, v3 = 5 };
+            mesh.Faces[6] = new Face { v1 = 2, v2 = 3, v3 = 7 };
+            mesh.Faces[7] = new Face { v1 = 3, v2 = 6, v3 = 7 };
+            mesh.Faces[8] = new Face { v1 = 0, v2 = 2, v3 = 7 };
+            mesh.Faces[9] = new Face { v1 = 0, v2 = 4, v3 = 7 };
+            mesh.Faces[10] = new Face { v1 = 4, v2 = 5, v3 = 6 };
+            mesh.Faces[11] = new Face { v1 = 4, v2 = 6, v3 = 7 };
+
+            _screen.Clear(0, 0, 0, 255);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
 
-        //Cyllinder
-        private void button_Copy2_Click(object sender, RoutedEventArgs e)
+        private void zUpButton_Click(object sender, RoutedEventArgs e)
         {
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y, mesh.Rotation.Z + 0.11f);
+            _screen.Render(camera, mesh);
+            _screen.Present();
+        }
 
+        private void zDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            _screen.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y, mesh.Rotation.Z - 0.11f);
+            _screen.Render(camera, mesh);
+            _screen.Present();
         }
     }
 }
---
+++ CG_Proj5/CG_Proj5/Utils.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using SharpDX;
+
+namespace CG_Proj5
+{
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Target { get; set; }
+    }
+    public struct Face
+    {
+        public int v1;
+        public int v2;
+        public int v3;
+    }
+    public class Mesh
+    {
+        public Vector3[] Vertices { get; private set; }
+        public Face[] Faces { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public Mesh(int verticesCount, int facesCount)
+        {
+            Vertices = new Vector3[verticesCount];
+            Faces = new Face[facesCount];
+        }
+    }
+
+    public class Screen
+    {
+        private byte[] backBuffer;
+        private WriteableBitmap bmp;
+
+        public Screen(WriteableBitmap bmp)
+        {
+            this.bmp = bmp;
+            backBuffer = new byte[bmp.PixelWidth * bmp.PixelHeight * 4];
+        }
+
+        public void DrawBresenham(Vector2 point0, Vector2 point1)
+        {
+            int x0 = (int)point0.X;
+            int y0 = (int)point0.Y;
+            int x1 = (int)point1.X;
+            int y1 = (int)point1.Y;
+
+            var dx = Math.Abs(x1 - x0);
+            var dy = Math.Abs(y1 - y0);
+            var sx = (x0 < x1) ? 1 : -1;
+            var sy = (y0 < y1) ? 1 : -1;
+            var err = dx - dy;
+
+            while (true)
+            {
+                DrawPoint(new Vector2(x0, y0));
+
+                if ((x0 == x1) && (y0 == y1)) break;
+                var e2 = 2 * err;
+                if (e2 > -dy) { err -= dy; x0 += sx; }
+                if (e2 < dx) { err += dx; y0 += sy; }
+            }
+        }
+
+        public void Clear(byte r, byte g, byte b, byte a)
+        {
+            for (var index = 0; index < backBuffer.Length; index += 4)
+            {
+                backBuffer[index] = b;
+                backBuffer[index + 1] = g;
+                backBuffer[index + 2] = r;
+                backBuffer[index + 3] = a;
+            }
+        }
+
+        public void Present()
+        {
+            bmp.Lock();
+            bmp.FromByteArray(backBuffer, 0, backBuffer.Length);
+            bmp.Unlock();
+        }
+
+        public void PutPixel(int x, int y, Color4 color)
+        {
+            var index = (x + y * bmp.PixelWidth) * 4;
+
+            backBuffer[index] = (byte)(color.Blue * 255);
+            backBuffer[index + 1] = (byte)(color.Green * 255);
+            backBuffer[index + 2] = (byte)(color.Red * 255);
+            backBuffer[index + 3] = (byte)(color.Alpha * 255);
+        }
+
+        public Vector2 Project(Vector3 coord, Matrix transMat)
+        {
+            var point = Vector3.TransformCoordinate(coord, transMat);
+            var x = point.X * bmp.PixelWidth + bmp.PixelWidth / 2.0f;
+            var y = -point.Y * bmp.PixelHeight + bmp.PixelHeight / 2.0f;
+            return (new Vector2(x, y));
+        }
+
+        public void DrawPoint(Vector2 point)
+        {
+            if (point.X >= 0 && point.Y >= 0 && point.X < bmp.PixelWidth && point.Y < bmp.PixelHeight)
+            {
+                PutPixel((int)point.X, (int)point.Y, new Color4(0.0f, 1.0f, 0.0f, 1.0f));
+            }
+        }
+
+        public void Render(Camera camera, params Mesh[] meshes)
+        {
+            var viewMatrix = Matrix.LookAtLH(camera.Position, camera.Target, Vector3.UnitY);
+            var projectionMatrix = Matrix.PerspectiveFovRH(0.78f,
+                                                           (float)bmp.PixelWidth / bmp.PixelHeight,
+                                                           0.01f, 1.0f);
+
+            foreach (Mesh mesh in meshes)
+            {
+                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y,
+                                                              mesh.Rotation.X, mesh.Rotation.Z) *
+                                  Matrix.Translation(mesh.Position);
+
+                var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
+
+                foreach (var vertex in mesh.Vertices)
+                {
+                    var point = Project(vertex, transformMatrix);
+                    DrawPoint(point);
+                }
+
+                foreach (var face in mesh.Faces)
+                {
+                    var vertexA = mesh.Vertices[face.v1];
+                    var vertexB = mesh.Vertices[face.v2];
+                    var vertexC = mesh.Vertices[face.v3];
+
+                    var pixelA = Project(vertexA, transformMatrix);
+                    var pixelB = Project(vertexB, transformMatrix);
+                    var pixelC = Project(vertexC, transformMatrix);
+
+                    DrawBresenham(pixelA, pixelB);
+                    DrawBresenham(pixelB, pixelC);
+                    DrawBresenham(pixelC, pixelA);
+                }
+            }
+        }
+    }
+}
